0dcdfb8a9cc01aa3322572051ea24e6854727e77
IT21276828 <99193625+IT21276828@users.noreply.github.com>
2022-02-14 17:43:30 +0000

    Tute 2 exe 1 completed

--- tute01.cpp
+++ tute01.cpp
@@ -33,7 +33,7 @@ int main()
 int main()
 {
   float cm, inches;
-  std::cout<<"Enter a length in cm : ";
+  std::cout<<"Enter a length in cm  : ";
   std::cin>>cm;
   inches = cm / 2.54;
   std::cout<<"Length in inches is"<<inches;
